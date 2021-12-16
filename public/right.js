36196f46254f3d88f276d19b53be0cc0f572622e
Savannah Leasman <leasman7@gmail.com>
2021-12-16 10:56:26 -0600

    updated file structure

--- public/right.js
+++ public/right.js
@@ -1,7 +1,7 @@
 function Right(props) {
     return (<>
         <aside>
-            <PlusMinus section="aside" handle={props.handle}/>
+            <PlusMinus section="right" handle={props.handle}/>
             <div className="section">Right:{props.data.right}</div>
             <Data data={props.data}/>
         </aside>
