f5ffae5bdb154f7fcd306243dbe4c3ef1f7c708f
Savannah Leasman <leasman7@gmail.com>
2021-12-16 14:44:07 -0600

    proper name now

--- key_get-set.js
+++
@@ -1,8 +0,0 @@
-var redis = require('redis');
-var client = redis.createClient();
-
-//single value write & read
-client.set("my_key", "Hellow World!");
-client.get("my_key", function(err, reply) {
-    console.log(reply);
-});
